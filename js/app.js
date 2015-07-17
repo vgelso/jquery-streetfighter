25cf5debc7de3acfbb97a2586066abb58bc02906
vgelso <gelsojobs@gmail.com>
2015-07-16 23:21:45 -0700

    Added jQuery events to Ryu

---
+++ js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').finish().show().animate(
+  	{'left': '1020px'},
+  	500,
+  	function() {
+  		$(this).hide();
+  		$(this).css('left', '520px');
+  		})
+  })
+  .mouseup(function() {
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  })
+});
+
+$(document).keydown(function(e) {
+	e.preventDefault();
+  	if (e.which == 88) {
+
+  		$('.ryu-cool').show();
+  		$('.ryu-still').hide();
+  	}
+  })
+	.keyup(function(e) {
+		if (e.which == 88) {
+			$('.ryu-cool').hide();
+  			$('.ryu-still').show();
+		}
+	});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+  
+	
